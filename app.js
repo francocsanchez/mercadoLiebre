2d7b140c62fdac2be0458262f644723ff5a187b8
Franco Sanchez <francocsanchez@gmail.com>
2021-08-26 15:17:24 -0300

    Micro desafio 2

---
+++ app.js
@@ -0,0 +1,19 @@
+//Require
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+//Public folder
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
+
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+//Server Up
+app.listen(3000, () => {
+    console.log('Server running');
+})
