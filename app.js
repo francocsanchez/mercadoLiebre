50cdea40ae607c4c1b4984e78b5d69909e6faa12
Franco Sanchez <francocsanchez@gmail.com>
2021-09-09 15:26:34 -0300

    Form user

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
 //Server Up
 app.listen(3000, () => console.log('Server running'))
